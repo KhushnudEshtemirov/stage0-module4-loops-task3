ecd469c89936183112126a288da82b522f667a31
Xushnud Eshtemirov <xushnud122997@gmail.com>
2024-11-20 11:53:37 +0500

    Done GCD, nines sum, fibonacci numbers

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int n1 = 0, n2 = 1, n3 = n1 + n2;
+        System.out.print(n1 + "\n" + n2 + "\n");
+        while (n3 < lastFibonacci) {
+            System.out.println(n3);
+            n1 = n2;
+            n2 = n3;
+            n3 = n1 + n2;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum1 = 0;
+        for (int i = 1; i <= lengthOfLastNumber; i++) {
+            int sum2 = 0;
+            for (int j = 0; j < i; j++) {
+                sum2 += 9 * Math.pow(10, j);
+            }
+            sum1 += sum2;
+        }
+        System.out.println(sum1);
     }
 }
